c6a0aa1ff1f5003d7a7e9d8275cd56bcdf9b4471
Farham <farhamapple@gmail.com>
2020-09-05 15:54:35 +0700

    StateFullComponent

---
+++ src/container/StateFullComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Class Compoenent
+class StateFullComponent extends React.Component{
+    render(){
+      return <p>Statefull Component</p>
+    }
+}
+
+export default StateFullComponent;
